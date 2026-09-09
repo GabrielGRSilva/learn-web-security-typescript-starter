250c610d1372cacc667b9c03281c9d7f13b5a0a9
Gabriel Silva <gabrielgrsilva@outlook.com.br>
2026-09-09 15:59:25 -0300

    feat: cookie expiration lesson 2/4

--- src/auth/sessionCookies.ts
+++ src/auth/sessionCookies.ts
@@ -2,19 +2,18 @@ import type { CookieOptions, Response } from "express";
 
 const SESSION_COOKIE_NAME = "session_id";
 
-const sessionCookieOptions = {
-  path: "/",
-} satisfies CookieOptions;
-
 type CookieSession = {
   token: string;
   expires_at: string;
 };
 
-export function setSessionCookie(
-  response: Response,
-  session: CookieSession,
-): void {
+const sessionCookieOptions = {
+  path: "/",
+  expires: new Date(0),
+} satisfies CookieOptions;
+
+export function setSessionCookie(response: Response, session: CookieSession,): void {
+  sessionCookieOptions.expires = new Date(session.expires_at);
   response.cookie(SESSION_COOKIE_NAME, session.token, sessionCookieOptions);
 }
 
