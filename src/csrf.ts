f67a6ced06caaab14f6b096bff26591345c3e6e0
Gabriel Silva <gabrielgrsilva@outlook.com.br>
2026-09-23 14:04:41 -0300

    feat: IN PROGRESS Cross-Site Request Forgery (CSRF) Lesson 6/3

--- src/csrf.ts
+++ src/csrf.ts
@@ -1,10 +1,29 @@
 import type { RequestHandler } from "express";
+import { sendErrorPage } from "./errors.ts";
 
-export function validateRequestOrigin(_appOrigin: string): RequestHandler {
-  return (_req, _res, next) => {
-    next();
+export function validateRequestOrigin(appOrigin: string): RequestHandler {
+  return (req, res, next) => {
+    if (req.method !== "POST") {
+      next();
+      return;
+    }
+
+    if (req.headers.origin && req.headers.origin !== appOrigin) {
+      return sendErrorPage(res, 403, "Forbidden", "External origin not allowed");
+    }
+    
+    try{
+      if (!req.headers.origin && req.headers.referer && req.headers.referer.startsWith(appOrigin)) {
+          next();
+      }else{
+        return sendErrorPage(res, 403, "Forbidden", "External origin not allowed");
+      }
+      
+    }catch (error) {
+        return sendErrorPage(res, 403, "Forbidden", "External origin not allowed");
+      }
+    }
   };
-}
 
 export function csrfTokensMatch(_expected: string, _actual: unknown): boolean {
   return true;
